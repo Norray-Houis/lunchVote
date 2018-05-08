1c32a20f24a5f95cf5e4ea84667ccbd5a332a8cd
Dev_001 <dev_001@MJ.local>
2018-05-08 18:13:06 +0800

    init

--- web/suites/config/database.php
+++ web/suites/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.0.0.1',
 	'username' => '9leaf_dc_user',
 	'password' => '123qwe',
 	'database' => '9thleaf_diancan',
